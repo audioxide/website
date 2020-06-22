23941344e85e88523927fc24ffd8fd9e8ecfb332
andrew.bridge <andrew.bridge@derivitec.com>
2020-06-22 23:32:34 +0100

    Implement album banner

--- components/AlbumBanner.vue
+++ components/AlbumBanner.vue
@@ -1,146 +1,34 @@
 <template>
     <div class="album-banner">
         <div class="album-banner__container">
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/james-blake-assume-form/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2019/01/Assume-Form-500x500.jpg" alt="Album cover for James Blake - Assume Form">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/david-bowie-hunky-dory/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2019/01/Hunky-Dory-500x500.jpg" alt="Album cover for David Bowie - Hunky Dory">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/the-rolling-stones-let-it-bleed/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2024/07/Let-It-Bleed-500x500.jpg" alt="Album cover for The Rolling Stones - Let It Bleed">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/system-of-a-down-toxicity/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2018/11/toxicity-500x500.jpg" alt="Album cover for System of a Down - Toxicity">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/a-tribe-called-quest-midnight-marauders/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2018/11/Midnight-Marauders-500x500.jpeg" alt="Album cover for A Tribe Called Quest - Midnight Marauders">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/vince-staples-fm/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2018/11/FM.jpg" alt="Album cover for Vince Staples - FM!">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/robyn-honey/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2018/11/Honey-500x500.jpg" alt="Album cover for Robyn - Honey">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/neneh-cherry-broken-politics/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2018/10/Broken-Politics-500x500.jpg" alt="Album cover for Neneh Cherry - Broken Politics">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/kurt-vile-bottle-it-in/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2018/10/Bottle-It-In-500x500.jpg" alt="Album cover for Kurt Vile - Bottle It In">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/queens-of-the-stone-age-queens-of-the-stone-age/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2018/10/Queens-of-the-Stone-Age.jpg" alt="Album cover for Queens of the Stone Age - Queens of the Stone Age">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/the-prodigy-the-fat-of-the-land/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/07/the-fat-of-the-land-500x500.jpeg" alt="Album cover for The Prodigy - The Fat of the Land">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/vince-staples-big-fish-theory/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/06/big-fish-theory-500x500.jpg" alt="Album cover for Vince Staples - Big Fish Theory">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/royal-blood-how-did-we-get-so-dark/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/06/how-did-we-get-so-dark-500x500.jpg" alt="Album cover for Royal Blood - How Did We Get So Dark?">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/oasis-definitely-maybe/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/06/definitely-maybe-500x500.jpg" alt="Album cover for Oasis - Definitely Maybe">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/alt-j-relaxer/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/06/Relaxer-500x500.jpg" alt="Album cover for alt-J - Relaxer">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/pixies-doolittle/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/05/doolittle-500x500.jpg" alt="Album cover for Pixies - Doolittle">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/soundgarden-superunknown/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/05/superunknown.jpg" alt="Album cover for Soundgarden - Superunknown">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/brian-eno-ambient-1-music-for-airports/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/05/Ambient-1.jpg" alt="Album cover for Brian Eno - Ambient 1: Music for Airports">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/run-the-jewels-run-the-jewels-2/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/05/RTJ2-500x500.jpg" alt="Album cover for Run the Jewels - Run the Jewels 2">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/gorillaz-humanz/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2017/04/Humanz-500x500.jpg" alt="Album cover for Gorillaz - Humanz">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/john-lennon-plastic-ono-band/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2016/03/John-Lennon-Plastic-Ono-Band-500x500.jpg" alt="Album cover for John Lennon - John Lennon/Plastic Ono Band">
-                </a>
-            </div>
-            <div class="album-banner__art-wrapper">
-                <a href="https://audioxide.com/reviews/the-life-of-pablo/" class="album-banner__review-link">
-                    <span class="album-banner__score">15</span>
-                    <img class="album-banner__album-art" src="https://audioxide.com/wp-content/uploads/2016/02/kanye-west-life-of-pablo-500x500.jpg" alt="Album cover for Kanye West - The Life of Pablo">
+            <div class="album-banner__art-wrapper" v-for="(album, key) in bannerData" :key="key">
+                <a :href="`/reviews/${album.slug}/`" class="album-banner__review-link">
+                    <span class="album-banner__score">{{ album.score }}</span>
+                    <img class="album-banner__album-art" :src="album.image" :alt="`Album cover for ${album.artist} - ${album.album}`">
                 </a>
             </div>
         </div>
     </div>
 </template>
 
-<script>
-export default {
-  name: 'AlbumBanner',
-};
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'AlbumBanner',
+    async created() {
+        // Add in an API endpoint that allows us to check if we have all the reviews
+        // if (!store.state.posts.posts.reviews) {
+            await this.$store.dispatch('getBannerData');
+        // }
+        return true;
+    },
+    computed: {
+        bannerData(): PostListing<Review> {
+            return this.$store.state.banner;
+        },
+    },
+});
 </script>
 
 <style lang="scss" scoped>
