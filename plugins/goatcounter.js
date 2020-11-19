fa7f57995a44482eb062dcb070cfb3985e4f8793
andrew.bridge <andrew.bridge@derivitec.com>
2020-11-19 00:56:56 +0000

    Always block onload event

--- plugins/goatcounter.js
+++ plugins/goatcounter.js
@@ -1,7 +1,7 @@
 export default ({ app }) => {
-    if (process.env.NETLIFY !== 'true' || process.env.CONTEXT !== 'production') return;
-
     window.goatcounter = { no_onload: true };
+
+    if (process.env.NETLIFY !== 'true' || process.env.CONTEXT !== 'production') return;
 
     app.router.afterEach((to, from) => {
         if (typeof window.goatcounter.count === 'function') {
