ee9659021820d50940ccc02e2f7a75a9ec90ef4c
Andrew Bridge <andrew.bridge@derivitec.com>
2020-11-19 00:30:16 +0000

    Add sitemap support (#93)

--- generateRoutes.js
+++ generateRoutes.js
@@ -1,13 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 const fetchPonyfill = require('fetch-ponyfill');
 const { fetch } = fetchPonyfill();
 
 // This is only run on servers and run before proxy redirects are implemented. Use the real API url
 const getData = (route) => fetch(`${process.env.EXT_API_URL}/${route}.json`).then(r => r.json());
 
+const cacheFilePath = path.resolve(__dirname, './routes.json');
+
 module.exports = async () => {
-    const routes = [];
+    const routes = ['/'];
     const types = await getData('types');
     types.pages.forEach(route => routes.push(`/${route}`));
+    if (fs.existsSync(cacheFilePath)) return JSON.parse(await fs.promises.readFile(cacheFilePath));
     await Promise.all(
         types.postTypes
             .map(type => routes.push(`/${type}`) && getData(type)
@@ -16,5 +21,6 @@ module.exports = async () => {
                 ),
             ),
     );
+    await fs.promises.writeFile(cacheFilePath, JSON.stringify(routes), { encoding: 'utf-8'});
     return routes;
 }
--- setup.js
+++ setup.js
@@ -1,32 +1,41 @@
 const path = require('path');
 const fse = require('fs-extra');
+const generateRoutes = require('./generateRoutes');
 
-const getPath = (...paths) => path.resolve(__dirname, ...paths);
-const getDepPath = (...paths) => getPath('./node_modules/@audioxide', ...paths);
-const getBrandDepPath = (...paths) => getDepPath('brand/assets', ...paths);
+const init = async () => {
+    const routesReady = generateRoutes();
 
-fse.copySync(
-    getDepPath('licensed-assets/webfonts'),
-    getPath('assets/webfonts'),
-);
+    const getPath = (...paths) => path.resolve(__dirname, ...paths);
+    const getDepPath = (...paths) => getPath('./node_modules/@audioxide', ...paths);
+    const getBrandDepPath = (...paths) => getDepPath('brand/assets', ...paths);
 
-const imgDir = getPath('assets/img');
-const staticDir = getPath('static');
-fse.copySync(
-    getBrandDepPath('svg'),
-    imgDir,
-);
+    fse.copySync(
+        getDepPath('licensed-assets/webfonts'),
+        getPath('assets/webfonts'),
+    );
 
-fse.copySync(
-    getBrandDepPath('favicon'),
-    staticDir,
-);
+    const imgDir = getPath('assets/img');
+    const staticDir = getPath('static');
+    fse.copySync(
+        getBrandDepPath('svg'),
+        imgDir,
+    );
 
-const brandPngsDep = getBrandDepPath('png');
-fse.readdirSync(brandPngsDep).forEach(item => {
-    if (item.startsWith('ribbon-')) {
-        fse.copySync(getPath(brandPngsDep, item), getPath(imgDir, item));
-    } else if (item.startsWith('social-tile') || item.startsWith('full-logo')) {
-        fse.copySync(getPath(brandPngsDep, item), getPath(staticDir, item));
-    }
-});
+    fse.copySync(
+        getBrandDepPath('favicon'),
+        staticDir,
+    );
+
+    const brandPngsDep = getBrandDepPath('png');
+    fse.readdirSync(brandPngsDep).forEach(item => {
+        if (item.startsWith('ribbon-')) {
+            fse.copySync(getPath(brandPngsDep, item), getPath(imgDir, item));
+        } else if (item.startsWith('social-tile') || item.startsWith('full-logo')) {
+            fse.copySync(getPath(brandPngsDep, item), getPath(staticDir, item));
+        }
+    });
+
+    await routesReady;
+}
+
+init();
