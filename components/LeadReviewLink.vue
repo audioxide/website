73c6395db7e24bdf228e1d6a5a9589e74aa23eb4
Frederick O'Brien <frederik.obrien@gmail.com>
2020-11-24 15:28:37 +0100

    Custom components for lead articles and reviews

---
+++ components/LeadReviewLink.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="post-link review-link">
+        <div class="img-wrap" :style="{ borderColor: post.metadata.colours[0] }">
+            <img v-if="image" :src="post.metadata.featuredimage[`${imageSize}-${imageFormat}`]" :alt="coverAlt" loading="lazy" width="300" height="300" />
+        </div>
+        <div class="info">
+            <h4 v-if="title">
+                <nuxt-link :to="`/${post.metadata.type}/${post.metadata.slug}`" class="overlay-link">
+                    <span class="album">{{ post.metadata.album }}</span>
+                    <span class="artist">{{ post.metadata.artist }}</span>
+                </nuxt-link>
+            </h4>
+            <p v-if="blurb">{{ post.metadata.summary }}</p>
+            <p v-if="author && post.metadata.author" class="author">By <span class="name">{{ post.metadata.author.name }}</span></p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue';
+import { albumCoverAlt } from '~/assets/utilities';
+import PostLink from './PostLink.vue';
+
+export default PostLink.extend({
+    name: 'LeadReviewLink',
+    props: {
+        imageFormat: { type: String as PropType<ImageSizeAspectRatios>, default: 'square' },
+    },
+    computed: {
+        coverAlt(): string {
+            return albumCoverAlt(this.post as Review);
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/styles/variables";
+
+@include postLink;
+@include postLinkFlex;
+
+h4 span {
+    display: block;
+}
+
+.img-wrap {
+    border: $line-width solid black;
+}
+
+.album {
+    font-style: italic;
+}
+</style>
