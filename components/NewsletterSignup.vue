1f136650e4c77b43bf496ae17ebe664f0788f712
Frederick O'Brien <frederik.obrien@gmail.com>
2021-02-04 14:39:48 +0100

    New newsletter signup prompt copy

--- components/NewsletterSignup.vue
+++ components/NewsletterSignup.vue
@@ -11,7 +11,7 @@
     >
       <div id="mc_embed_signup_scroll">
         <div class="prompt">
-        <span>Receive our monthly roundups.</span>
+        <span>Music is best shared, don’t you think? Sign up for monthly roundups of what we’ve been listening to.</span>
         </div>
         <div class="signup">
         <div class="mc-field-group">
