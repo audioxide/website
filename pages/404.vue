0d78e8fbfd427899ed83923895e0f5e6042fa490
Frederick O'Brien <frederik.obrien@gmail.com>
2021-02-14 00:46:19 +0100

    404 attempt

---
+++ pages/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h2>Page could not be found</h2>
+    <p>Go to <a href="/">the home page</a> or try your luck with <a href="/random/">a random post</a>.</p>
+  </main>
+</template>
+
+<script lang="ts">
+</script>
+
+<style>
+    @import "~assets/styles/animations.scss";
+</style>
