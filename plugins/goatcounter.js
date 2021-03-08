565000eb7b98e531afba1ee208d6de96327ca9ca
Andrew Bridge <andrew@andrewhbridge.co.uk>
2021-03-08 21:07:19 +0000

    Ensure views aren't miscounted with multiple route changes in quick succession

--- plugins/goatcounter.js
+++ plugins/goatcounter.js
@@ -1,14 +1,19 @@
 export default ({ app }) => {
     window.goatcounter = { no_onload: true };
+    let timeoutUID;
 
     app.router.afterEach((to, from) => {
         if (process.env.isProduction !== true) {
             console.log(`Count pageview for '${to.fullPath}'`);
         } else if (typeof window.goatcounter.count === 'function') {
             // TODO: Nasty temporary hack to wait for title change, improve
-            setTimeout(() => window.goatcounter.count({
+            if (typeof timeoutUID === 'number') clearTimeout(timeoutUID);
+            timeoutUID = setTimeout(() => {
+                timeoutUID = null;
+                window.goatcounter.count({
                     path: to.fullPath,
-                }), 1000);
+                });
+            }, 1000);
         }
     });
 }
