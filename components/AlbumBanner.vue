b2f7706275b10867ba475658d9cb63c907445318
Frederick O'Brien <frederik.obrien@gmail.com>
2021-04-04 19:24:48 +0200

    Update AlbumBanner.vue

--- components/AlbumBanner.vue
+++ components/AlbumBanner.vue
@@ -76,7 +76,7 @@ export default Vue.extend({
         background: rgba(0,0,0,0.6);
         color: white;
         font-family: $logo-fontstack;
-        font-size: 6vw;
+        font-size: 4vw;
         text-shadow: 0px 4px 3px rgba(0,0,0,0.4),
         0px 8px 13px rgba(0,0,0,0.1),
         0px 18px 23px rgba(0,0,0,0.1);
