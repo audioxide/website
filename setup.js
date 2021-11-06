ad76e43f7c92f42b81dcbc7e19233e0412ce6790
Frederick O'Brien <frederik.obrien@gmail.com>
2021-11-06 16:16:13 +0000

    Purging FA (and updating some dependencies)

--- setup.js
+++ setup.js
@@ -10,7 +10,6 @@ const init = async () => {
     const getBrandDepPath = (...paths) => getDepPath('brand/assets', ...paths);
 
     fse.copySync(
-        getDepPath('licensed-assets/webfonts'),
         getPath('assets/webfonts'),
     );
 
