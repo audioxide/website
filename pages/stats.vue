2cc3ff3f17d63f9c1961162f153639b5b544591d
Frederick O'Brien <frederik.obrien@gmail.com>
2021-12-18 00:52:47 +0000

    Fetch data in Stats.vue then pass into components

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,28 +1,36 @@
 <template>
   <main>
     <h2>Stats</h2>
-    <stats-overview />
+    <stats-overview :reviewData="{ data }" />
   </main>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import StatsOverview from '@/components/StatsOverview.vue';
+import Vue from 'vue'
+import StatsOverview from '@/components/StatsOverview.vue'
 
 export default Vue.extend({
+  data: function() {
+    return fetch(
+      'https://gist.githubusercontent.com/frederickobrien/6c2239358cfa04d6aaf5f2275a864e56/raw/81e4a925db42361eaf1be69d3a1adfc94795ecec/reviews-data.json'
+    )
+      .then((response) => response.json())
+      .then((data) => data)
+  },
   components: { StatsOverview }
 })
-
 </script>
 
 <style lang="scss" scoped>
-    @import "~assets/styles/variables";
+@import '~assets/styles/variables';
 
-    h2 {
-        text-align: center;
-        font-size: 2em;
-        font-family: $heading-fontstack;
-        padding: 30px;
-    }
-
+h2 {
+  text-align: center;
+  font-size: 3em;
+  font-family: $heading-fontstack;
+  margin-top: 1em;
+}
 </style>
+
+function fetchData() { throw new Error('Function not implemented.') } function
+fetchData() { throw new Error('Function not implemented.') }
