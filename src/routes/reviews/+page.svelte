b489845fcc61da67f2f8c30049681065900a8461
Frederick O'Brien <frederik.obrien@gmail.com>
2024-09-09 22:42:12 +0100

    Tidier review listings page

--- src/routes/reviews/+page.svelte
+++ src/routes/reviews/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import PostSummaryCard from '$lib/components/PostSummaryCard.svelte';
 	import { SITE_NAME } from '$lib/constants';
 
 	export let data;
@@ -9,10 +10,31 @@
 	<meta name="description" content="Music reviews, articles, and interviews." />
 </svelte:head>
 
-<h1>Reviews</h1>
-{#each data.reviews as review}
-	<a href={`/reviews/${review.metadata.slug}`}>
-		<h2>{review.metadata.title}</h2>
-		<p>{review.metadata.artist}</p>
-	</a>
-{/each}
+<h2>Album Reviews</h2>
+<div class="reviews-container">
+	{#each data.reviews as review}
+		<PostSummaryCard
+			image={review.metadata.featuredimage['medium-square']}
+			title={review.metadata.title}
+			link={`/reviews/${review.metadata.slug}`}
+			blurb={review.metadata.blurb}
+			primaryColor={review.metadata.colours ? review.metadata.colours[0] : 'lightgray'}
+		/>
+	{/each}
+</div>
+
+<style>
+	h2 {
+		font-family: 'Spectral SC', serif;
+		font-weight: 300;
+		font-size: 1.2rem;
+		font-style: italic;
+		text-align: center;
+		margin-bottom: 2rem;
+	}
+	.reviews-container {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+		gap: 2rem;
+	}
+</style>
