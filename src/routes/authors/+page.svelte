e6369e28ff02fa2747c7a5f6065bb49efd002dc4
Frederick O'Brien <frederik.obrien@gmail.com>
2024-09-16 22:56:04 +0100

    Author meta descriptions

--- src/routes/authors/+page.svelte
+++ src/routes/authors/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import { SITE_NAME } from '$lib/constants';
+
 	let { data } = $props();
 
 	const authorKeys = Object.keys(data.authors);
 </script>
+
+<svelte:head>
+	<title>Authors // {SITE_NAME}</title>
+	<meta name="description" content="A list of all authors on {SITE_NAME}" />
+</svelte:head>
 
 <h2>Authors</h2>
 
--- src/routes/authors/[slug]/+page.svelte
+++ src/routes/authors/[slug]/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import { SITE_NAME } from '$lib/constants';
+
 	let { data } = $props();
 </script>
+
+<svelte:head>
+	<title>{data.author?.name} // {SITE_NAME}</title>
+	<meta name="description" content={`Author profile for ${data.author?.name}`} />
+</svelte:head>
 
 <h2>{data.author?.name}</h2>
 
