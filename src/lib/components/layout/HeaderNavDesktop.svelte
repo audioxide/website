27abc0c3156e4141b4b5afb5426603dafe0559e2
Frederick O'Brien <frederik.obrien@gmail.com>
2024-11-09 21:19:34 +0000

    Expanding sublinks on desktop

--- src/lib/components/layout/HeaderNavDesktop.svelte
+++ src/lib/components/layout/HeaderNavDesktop.svelte
@@ -2,18 +2,57 @@
 	import type { NavSection } from '$lib/types/navigation';
 
 	let { links }: { links: NavSection[] } = $props();
+
+	let displaySubLinks = $state(false);
+
+	const toggleSubLinks = () => {
+		displaySubLinks = !displaySubLinks;
+	};
 </script>
 
 <nav>
-	<ul>
+	<ul class="pillars">
 		{#each links as item}
 			<li>
-				{#if item.name !== 'More'}
-					<a href={item.link}>{item.name}</a>
+				{#if item.name === 'More'}
+					<div
+						class="more"
+						onclick={toggleSubLinks}
+						onkeydown={toggleSubLinks}
+						tabindex="0"
+						role="button"
+					>
+						{item.name}
+						{displaySubLinks ? '▲' : '▼'}
+					</div>
+				{:else}
+					<a
+						href={item.link}
+						onclick={() => {
+							if (displaySubLinks) {
+								toggleSubLinks();
+							}
+						}}>{item.name}</a
+					>
 				{/if}
 			</li>
 		{/each}
 	</ul>
+	{#if displaySubLinks}
+		<div class="sub-links">
+			{#each links as item}
+				{#if item.subLinks}
+					<ul>
+						{#each item.subLinks as link}
+							<li><a href={link.link} onclick={toggleSubLinks}>{link.name}</a></li>
+						{/each}
+					</ul>
+				{:else}
+					<ul></ul>
+				{/if}
+			{/each}
+		</div>
+	{/if}
 </nav>
 
 <style>
@@ -22,16 +61,41 @@
 		color: #fff;
 		text-decoration: none;
 	}
-	nav {
+	.pillars,
+	.sub-links {
 		display: flex;
 		flex-direction: row;
-		gap: 1rem;
-		align-items: center;
 	}
-	ul {
-		display: flex;
-		flex-direction: row;
-		gap: 3rem;
+	.pillars li,
+	.sub-links ul {
+		width: 170px;
+	}
+	.sub-links {
+		margin-top: 0.5rem;
+		position: absolute;
+		background-color: black;
+		z-index: 100;
+		padding: 0 0 1rem 0;
+	}
+	.sub-links::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: -50px;
+		width: 100vw;
+		height: 100%;
+		background-color: black;
+		z-index: -1;
+		overflow: hidden;
+	}
+	.sub-links li {
+		padding: 0.4rem 0;
+	}
+	.more {
+		color: white;
+		&:hover {
+			cursor: pointer;
+		}
 	}
 	nav {
 		display: none;
