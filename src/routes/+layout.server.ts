392bb8cbcd4a1e2b72d4c637a1e464b93cae4622
Frederick O'Brien <frederik.obrien@gmail.com>
2024-12-07 15:05:03 +0000

    Remove replace from article content, add banner type

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,9 @@
 import { API_URL } from '$lib/constants';
+import type { BannerAlbums } from '$lib/types/shared.js';
 
 export const load = async ({ fetch }) => {
 	const response = await fetch(`${API_URL}/albumbanner.json`);
-	const bannerReviews = await response.json();
+	const bannerReviews: BannerAlbums = await response.json();
 	return {
 		bannerReviews
 	};
