89c0bb78293bf4521d345f67df8fadcc707d31a2
Frederick O'Brien <frederik.obrien@gmail.com>
2024-12-11 22:41:23 +0000

    Use `entries` to pre-render static pages

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,16 +1,14 @@
 import { API_URL } from '$lib/constants';
 
-export async function load({ fetch, params }) {
-	const pageResponse = await fetch(`${API_URL}/pages/${params.slug}.json`);
-	const page: {
-		metadata: {
-			slug: string;
-			title: string;
-			blurb: string;
-			created: string;
-			modified: string;
-		};
-		content: string[];
-	} = await pageResponse.json();
-	return { page };
+export async function entries() {
+	const typesResponse = await fetch(`${API_URL}/types.json`);
+	const types: {
+		pages: string[];
+		postTypes: string[];
+		postTotal: number;
+	} = await typesResponse.json();
+
+	const slugParams = types.pages.map((slug) => ({ slug }));
+
+	return slugParams;
 }
