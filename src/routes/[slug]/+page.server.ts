5424259f7a93e62f9c830977b45d4d8fe906bcd5
Frederick O'Brien <frederik.obrien@gmail.com>
2024-12-11 22:49:03 +0000

    Put `entries` and `load` in same file
    Feels much tider

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -12,3 +12,18 @@ export async function entries() {
 
 	return slugParams;
 }
+
+export async function load({ fetch, params }) {
+	const pageResponse = await fetch(`${API_URL}/pages/${params.slug}.json`);
+	const page: {
+		metadata: {
+			slug: string;
+			title: string;
+			blurb: string;
+			created: string;
+			modified: string;
+		};
+		content: string[];
+	} = await pageResponse.json();
+	return { page };
+}
